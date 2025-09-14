13822c9d49fbb9d6f263eec7f8df075baecb2154
Carlos Vilela <carlos.vilela@users.noreply.github.com>
2025-09-14 20:28:27 -0300

    Project Initiation

---
+++ app/src/main/java/it/codexplo/app_alpha_mobile_utility_basiccalculator_001/MainActivity.kt
@@ -0,0 +1,47 @@
+package it.codexplo.app_alpha_mobile_utility_basiccalculator_001
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.codexplo.app_alpha_mobile_utility_basiccalculator_001.ui.theme.App_alpha_mobile_utility_basiccalculator_001Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            App_alpha_mobile_utility_basiccalculator_001Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    App_alpha_mobile_utility_basiccalculator_001Theme {
+        Greeting("Android")
+    }
+}
